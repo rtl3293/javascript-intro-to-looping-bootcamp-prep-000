33ac23a9231c714dfb0e2c86cf0493b7ac925e38
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 22:17:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,4 +16,11 @@ function whileLoop(n) {
   return 'done'
 }
 
-whileLoop(100)
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+  }
+  do {
+
+  } while(array.length > 0 && maybeTrue())
+}
