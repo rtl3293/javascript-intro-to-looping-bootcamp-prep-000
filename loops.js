c21149d41efc2f75b8e5f34307b7c0d0c85cdee6
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 22:02:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array) {
   for (var i = 1; i <= 25; i++) {
     if (i === 1) {
-      array.unshift("I am 1 strange loop.")
+      array.pop("I am 1 strange loop.")
     } else {
       array.unshift(`I am ${i} strange loops.`)
     }
