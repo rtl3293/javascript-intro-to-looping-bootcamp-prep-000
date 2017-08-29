dc9f06463e09b24c897507328dc30cb0d13ac31a
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 21:57:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,9 @@
-for (var i = 1; i <= 100; i++) {
-  console.log("Hello World the " + i + " time");
+function forLoop(array) {
+  for (var i = 0; i <= 25; i++) {
+    if i === 1 {
+      console.log("I am 1 strange loop.")
+    } else {
+      console.log(`I am ${i} strange loops.`)
+    }
+  }
 }
